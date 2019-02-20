37aae0357bf027fe2d3ee35c0cab7ce152f2244c
Ryan James <james.ryan.d@outlook.com>
2019-02-20 10:22:34 -0500

    Working more

--- Mathematical Formulas/Program.cs
+++ Mathematical Formulas/Program.cs
@@ -12,7 +12,7 @@ namespace Mathematical_Formulas
         {
             Console.WriteLine ("Welcome! I am the Geometry Guru. I will help you calculate!");
             Console.Write("Let's find the area and circumference of a circle.");
-            double radius = 4.0;
+            double radius = 4.0 ;
                 
             double area = Math.PI * Math.Pow(radius, 2);
             double circumference = 2 * Math.PI * 4;
